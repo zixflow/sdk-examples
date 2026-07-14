3dfe0a002e10f3907548be45c8b23488635fe7c3
Sahil Khaire <sahil@zixflow.com>
2026-07-14 10:24:34 +0530

    chore: add builtInKotlin and newDsl flags to gradle.properties for Flutter migration

---
+++ flutter/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<zixflow/ZixflowPlugin.h>)
+#import <zixflow/ZixflowPlugin.h>
+#else
+@import zixflow;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ZixflowPlugin registerWithRegistrar:[registry registrarForPlugin:@"ZixflowPlugin"]];
+}
+
+@end
